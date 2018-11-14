c75d7667ecdabb7877d6591360ed7537e83d2aae
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2018-11-14 14:37:25 -0800

    lucky 7's

--- Guess Game.py
+++ Guess Game.py
@@ -1,7 +1,7 @@
 import random
-answer = random.radint (1, 10)
+
 guesses = 5
 playing = True
-while guesses > 0 and playing:
-    guess = int (input("type in an number"))
-    if
+random_integer = random.randint(1, 10)
+while playing and guesses > 5:
+    guess = imput
--- Lucky 7's.py
+++ Lucky 7's.py
@@ -1,6 +1,7 @@
 import random
 mymoney = 15
-diceroll1 = random.rantint (1, 6)
+diceroll1 = random.randint (1, 6)
 diceroll2 = random.randint(1, 6)
 answer = diceroll1 + diceroll2
-if 
+if answer  == 7:
+    print("YOU WIN!!!!!!")
