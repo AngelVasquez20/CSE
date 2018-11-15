b6b8b2bfdf6d2b84671da9cce3bd6e9047c6b59d
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2018-11-15 14:34:33 -0800

    11/15/18

--- Guess Game.py
+++ Guess Game.py
@@ -3,5 +3,16 @@ import random
 guesses = 5
 playing = True
 random_integer = random.randint(1, 10)
-while playing and guesses > 5:
-    guess = imput
+while playing and guesses > 0:
+    guess = input("Choose a number from 1 and 10")
+    if int(guess) == int(random_integer):
+        print("You have won!!")
+        playing = False
+
+    else:
+        print("Try Again!!")
+        guesses -= 1
+    if int(guess) > int(random_integer):
+        print("Too High")
+    if int(guess) < int(random_integer):
+        print("Too Low")
