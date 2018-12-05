7de5fa407fa836b0f5f2943a7801f54a5454217a
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2018-12-05 14:37:50 -0800

    12/5/18

--- Angel Vasquez-Challenges.py
+++ Angel Vasquez-Challenges.py
@@ -1,5 +1,24 @@
+# Challenge number 1
 
-def challenge1 (firstname, lastname):
+
+def challenge1(firstname, lastname):
     print(lastname, firstname)
 
+
 challenge1("John", "Doe")
+
+
+# Challenge number 4
+
+
+def challenge4(number):
+    if number < 0:
+        return "Negative"
+    if number == 0:
+        return "Zero"
+    if number > 0:
+        return "positive"
+
+
+print(challenge4(10))
+
