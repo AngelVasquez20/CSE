ffd0bc7ac3df8d777fe2d6136bb478766e6eeb5e
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2018-12-06 14:38:20 -0800

    12/6/18

--- Angel Vasquez-Challenges.py
+++ Angel Vasquez-Challenges.py
@@ -22,3 +22,35 @@ def challenge4(number):
 
 print(challenge4(10))
 
+
+# Challenge number 3
+
+
+def challenge3(b, h):
+    return 5 * b * h
+
+
+print(challenge3(3, 6))
+
+
+# Challenge number 5
+
+
+def challenge5(radius):
+
+    return 3.14 * radius ** 2
+
+
+print(challenge5(6))
+
+
+# Challenge number 6
+
+
+def challenge6(radius):
+    return 4/3 * 3.14 * radius ** 3
+
+
+print(challenge6(7))
+
+volume1 = ("a", "e", "i")
