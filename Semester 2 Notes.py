a2145214c66b93a977bee10965d53f1740d05e45
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-09 14:08:13 -0800

    1/9/19

---
+++ Semester 2 Notes.py
@@ -0,0 +1,9 @@
+age = input("How old are you")
+print("%s?? Really???" % age)
+
+colors = ["Red", "Blue", "Black", "Yellow", "orange"]
+colors.append("cyan")
+colors.pop(0)
+print(colors)
+print(colors[1])
+print(len(colors))
