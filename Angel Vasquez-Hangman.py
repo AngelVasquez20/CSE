04dbe1bc9c35a19af090bda9d3d927fb67bed8e3
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-11 14:50:18 -0800

    1/11/19

--- Angel Vasquez-Hangman.py
+++ Angel Vasquez-Hangman.py
@@ -1,3 +1,11 @@
 word = ["Apple", "Words", "Backpack", "Pencil", "Legs", "Pants", "Dog", "Cat", "Guitar", "Shoe"]
 print(word)
 
+
+string1 = "turquoise"
+list1 = list(string1)
+print(list1)
+for i in range(len(list1)):
+    if list1[i] == "u":
+        list1.pop(i)
+        list1.insert(i, "*")
