4ebfafc859da68414a83388303b3d4f5b5b66e79
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-14 14:37:44 -0800

    1/14/19

--- Angel Vasquez-Hangman.py
+++ Angel Vasquez-Hangman.py
@@ -1,11 +1,6 @@
-word = ["Apple", "Words", "Backpack", "Pencil", "Legs", "Pants", "Dog", "Cat", "Guitar", "Shoe"]
-print(word)
+import random
 
+guess = 8
 
-string1 = "turquoise"
-list1 = list(string1)
-print(list1)
-for i in range(len(list1)):
-    if list1[i] == "u":
-        list1.pop(i)
-        list1.insert(i, "*")
+words = ["Apple", "Words", "Backpack", "Pencil", "Legs", "Pants", "Dog", "Cat", "Guitar", "Shoe"]
+random = random.choice(words)
--- Angel Vasquez-Lucky 7's.py
+++ Angel Vasquez-Lucky 7's.py
@@ -3,7 +3,7 @@ mymoney = 15
 rounds = 0
 
 while mymoney > 0:
-    diceroll1 = random.randint (1, 6)
+    diceroll1 = random.randint(1, 6)
     diceroll2 = random.randint(1, 6)
     answer = diceroll1 + diceroll2
     rounds += 1
