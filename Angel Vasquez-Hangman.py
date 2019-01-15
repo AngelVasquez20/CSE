153d3c25b2b922a51528e6175115dc3b7ce35999
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-15 14:37:51 -0800

    1/15/17

--- Angel Vasquez-Hangman.py
+++ Angel Vasquez-Hangman.py
@@ -1,6 +1,4 @@
 import random
 
-guess = 8
-
-words = ["Apple", "Words", "Backpack", "Pencil", "Legs", "Pants", "Dog", "Cat", "Guitar", "Shoe"]
-random = random.choice(words)
+words = ["Apple", "Word", "Backpack", "Pencil", "Legs", "Mouse", "Dog", "Cat", "Guitar", "Shoe", "I love dogs!"]
+random = random.choice(words)   
--- Semester 2 Notes.py
+++ Semester 2 Notes.py
@@ -1,3 +1,4 @@
+import string
 age = input("How old are you")
 print("%s?? Really???" % age)
 
@@ -7,3 +8,12 @@ colors.pop(0)
 print(colors)
 print(colors[1])
 print(len(colors))
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+# hangman
+output = word
+word = word
+word_letters = ['w', 'o', 'r', 'd']
