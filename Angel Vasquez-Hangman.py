f1664d09eded4f17f4ce82615969076fe290d86f
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-16 13:45:40 -0800

    1/15/19

--- Angel Vasquez-Hangman.py
+++ Angel Vasquez-Hangman.py
@@ -1,4 +1,4 @@
 import random
 
 words = ["Apple", "Word", "Backpack", "Pencil", "Legs", "Mouse", "Dog", "Cat", "Guitar", "Shoe", "I love dogs!"]
-random = random.choice(words)   
+random = random.choice(words)
