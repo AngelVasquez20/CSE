68410d0afc3842f2d88ad9a80175c72b5768b5f1
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-25 14:35:29 -0800

    1/25/19

--- Hangman- Angel Vasquez.py
+++ Hangman- Angel Vasquez.py
@@ -1,21 +1,9 @@
 import random
-guesses = 8
 words = ["Apple", "Word", "Backpack", "Pencil", "Legs", "Mouse", "Dog", "Cat", "Myth", "Shoe", "I love dogs!"]
 random = random.choice(words)
 display = []
-for i in range(len(display)):
-    display[i] = "_"
-print(' '.join(display))
+
+for i in range(len(words)):
+    words[i] = "_"
+print(' '.join(words))
 print()
-
-playing = True
-
-while playing and guesses > 0:
-    guess = input("Guess a letter: ")
-    if int(guess) == int(random):
-        print("You guesses the word")
-        playing = False
-
-    else:
-        print("try again")
-        guesses -= 1
