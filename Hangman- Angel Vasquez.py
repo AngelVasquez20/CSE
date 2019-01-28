475139776960044566c721fcadf872f6d5977284
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-28 14:36:22 -0800

    1/28/19

--- Hangman- Angel Vasquez.py
+++ Hangman- Angel Vasquez.py
@@ -1,9 +1,34 @@
 import random
+guess = 8
+puncuation = string.punctuation
+string =
+list1 = list(string)
+print(list1)
+alphabet = list(string.ascii_letters)
 words = ["Apple", "Word", "Backpack", "Pencil", "Legs", "Mouse", "Dog", "Cat", "Myth", "Shoe", "I love dogs!"]
 random = random.choice(words)
-display = []
+letters_guessed = []
 
 for i in range(len(words)):
-    words[i] = "_"
-print(' '.join(words))
+    if words[i] in alphabet:
+        list1.pop(i)
+        list1.insert(i, "_")
+
+print("".join(words))
 print()
+
+while guess > 0:
+    guess = input("Guess a letter: ")
+    print(guess)
+    letters_guessed.append(guess.lower())
+    for i in range(len(words)):
+        if words[i].lower() in letters_guessed:
+            list1.pop(i)
+            list1.insert(i, words[i])
+
+if guess.lower() not in list1 and guess.upper() not in list1:
+    guess -= 1
+    print("".join(list1))
+if "_" not in list1:
+    win = True
+    print("Well done winner you have guessed the word.")
