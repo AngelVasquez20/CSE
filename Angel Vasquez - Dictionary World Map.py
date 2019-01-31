aeff5480d5c8b630477c10721f9d0ba0b04f4464
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-01-31 14:38:16 -0800

    1/31/19

--- Angel Vasquez - Dictionary World Map.py
+++ Angel Vasquez - Dictionary World Map.py
@@ -1,20 +1,40 @@
 world_map = {
     'R19A': {
-        'NAME': "Mr. Wiebe's room",
-        'DESCRIPTION': "This is the room that you are in",
-        'PATHS': "", {
-            'NORTH': "Parking_Lot"
-
+        'NAME': "Wiebe's Classroom",
+        'DESCRIPTION': "This is the classroom that you are in right "
+                       "now. It has two exits to the north side.",
+        'PATHS': {
+            'NORTH': "PARKING_LOT"
         }
     },
-
-
     'PARKING_LOT': {
-        'NAME': "",
-        'DESCRIPTION': "",
-        'PATH': {
-
+        'NAME': "The Edison Parking Lot",
+        'DESCRIPTION': "There are cars parked here. To "
+                       "the south is Mr. Wiebe's room",
+        'PATHS': {
+            'SOUTH': "R19A"
         }
     }
-
 }
+
+# Other Variables
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+current_node = world_map['R19A']   # this is your current location
+playing = True
+
+# Controller
+while playing:
+    print(current_node['NAME'])
+
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+
+    else:
+        print("Command not recognized")
--- Hangman- Angel Vasquez.py
+++ Hangman- Angel Vasquez.py
@@ -1,9 +1,8 @@
 import random
 import string
-win = False
+
 guess = 8
 pun = list(string.punctuation)
-list1 = list()
 alphabet = list(string.ascii_letters)
 words = ["Apple",
          "Word",
@@ -16,32 +15,30 @@ words = ["Apple",
          "Myth",
          "Shoe",
          "I love dogs!"]
-
 random = random.choice(words)
+random_word = list(random)
 letters_guessed = []
 
-
+win = False
 for i in range(len(random)):
     if random[i] in alphabet:
-        list1.pop(i)
-        list1.insert(i, "_")
+        random_word.pop(i)
+        random_word.insert(i, "_")
     if random[i] in pun:
-        list1.insert(i, "!")
-print("".join(list1))
-print(' '.join(words))
+        random_word.insert(i, "!")
+print(' '.join(random_word))
 
 while guess > 0 and not win:
     guess1 = input("Guess a letter: ")
     print(guess)
     letters_guessed.append(guess1.lower())
-    for i in range(len(words)):
+    for i in range(len(random)):
         if random[i].lower() in letters_guessed:
-            list1.pop(i)
-            list1.insert(i, words[i])
-
-if guess1.lower() not in list1 and guess1.upper() not in list1:
-    guess -= 1
-    print("".join(list1))
-if "_" not in list1:
+            random_word.pop(i)
+            random_word.insert(i, random[i])
+    if guess1.lower() not in random_word and guess1.upper() not in random_word:
+        guess -= 1
+    print("".join(random_word))
+if "_" not in random_word:
     win = True
     print("Well done winner you have guessed the word.")
