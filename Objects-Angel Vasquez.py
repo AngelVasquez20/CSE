b080da87f99019998e1f60f521062cc461ccf2f7
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-02-15 14:37:33 -0800

    2/15/19

--- Objects-Angel Vasquez.py
+++ Objects-Angel Vasquez.py
@@ -1,23 +1,39 @@
 class Car(object):
-    def __init__(self, power, leather, steel, rubber, color="blue"):
+    def __init__(self, leather, space=82, color="blue"):
         self.engine = "Jaguar AJ-V8"
         self.leather = leather
-        self.steel = steel
-        self.rubber = rubber
+        self.trunk_space = space
         self.gas = 100
         self.color = color
         self.functioning = True
-        self.power_left = power
 
     def fill_gas(self, time):
         if self.functioning:
             if self.gas >= 100:
-                print("The car is filling up with gas")
+                print("The car is already filled up with gas")
                 return
 
-            self.power_left += time
-            if self.power_left > 100:
-                print("The car will fill up in %d" % self.power_left)
-                self.power_left = 100
+            self.gas += time
+            if self.gas > 100:
+                print("The car will fill up in %d minutes" % self.gas)
+                self.gas = 100
             else:
-                print("Your car is now %d low" % self.power_left)
+                print("Your car is now %d low" % self.gas)
+        else:
+            print("Great I have Arrived to my destination!")
+
+    def drive(self):
+        self.functioning = False
+        print("I took the keys to the car")
+        print("Then I drove it to my cousin's house.")
+
+    def use(self, time):
+        self.gas -= time
+        print("You use the car for %s minutes," % time)
+
+
+my_car = Car("Black", 82, "blue")
+
+my_car.fill_gas(20)
+my_car.use(60)
+my_car.drive()
