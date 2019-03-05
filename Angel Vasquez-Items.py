6d58ad8009c808dbdb6e5d7002f15d4ba3fb1c4e
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-03-05 14:37:41 -0800

    3/5/19

--- Angel Vasquez-Items.py
+++ Angel Vasquez-Items.py
@@ -2,3 +2,24 @@ class Item(object):
     def __init__(self, name):
         self.name = name
 
+
+class Sword(Item):
+    def __init__(self, name):
+        super(Sword, self).__init__(name)
+        self.damage = 100
+        self.health = 100
+
+    def swinging(self):
+        self.health -= 1
+        print("You are swinging the door the sword")
+
+
+class Iron(Sword):
+    def __init__(self):
+        super(Iron, self).__init__("Iron")
+
+
+class Shield(Item):
+    def __init__(self, name):
+        super(Shield, self).__init__(name)
+        self.health_left = 100
