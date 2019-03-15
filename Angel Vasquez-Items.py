b63f9585227e8c9d4c5654a773af60adea8cf1b4
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-03-15 14:46:59 -0700

    Update Angel Vasquez-Items.py

--- Angel Vasquez-Items.py
+++ Angel Vasquez-Items.py
@@ -220,12 +220,13 @@ class Ak47(Gun):
         super(Ak47, self).__init__("Ak47")
         self.range = 1000
         self.damage = 100
+        self.bullets_left = 100
 
     def info(self):
         self.damage = 100
         self.range = 1000
         self.bullets_left = 100
-        print("Your gun does 100 damage, 1000 range, and 100 bullets")
+        print("The AK47 does 100 damage, has 1000 range, and has 100 bullets")
 
     def shoot(self):
         self.bullets_left -= 10
@@ -238,6 +239,16 @@ class Pistol(Gun):
         self.range = 100
         self.damage = 50
 
+    def info(self):
+        self.damage = 50
+        self.range = 100
+        self.bullets_left = 100
+        print("The Pistol does 50 damage, has 100 range, and has 100 bullets")
+
+    def shoot(self):
+        self.bullets_left -= 10
+        print("You shoot the Pistol, you have %s bullets left" % self.bullets_left)
+
 
 class Sniper(Gun):
     def __init__(self):
@@ -245,31 +256,57 @@ class Sniper(Gun):
         self.range = 10000
         self.damage = 1000
 
+    def info(self):
+        self.damage = 1000
+        self.range = 10000
+        self.bullets_left = 100
+        print("The Sniper does 1000 damage, has 10000 range, and has 100 bullets")
+
+    def shoot(self):
+        self.bullets_left -= 10
+        print("You shoot the Sniper, you have %s bullets left" % self.bullets_left)
+
 
 class Clothes(Item):
     def __init__(self, name):
         super(Clothes, self).__init__(name)
-        self.protection = 1
+        self.protection = 3
+
+    def info(self):
+        self.protection = 3
+        print("Your clothes has 1 protection")
 
 
 class Pants(Clothes):
     def __init__(self):
         super(Pants, self).__init__("Pants")
 
+    def info(self):
+        print("Pants has 3 protection")
+
 
 class Shirt(Clothes):
     def __init__(self):
         super(Shirt, self).__init__("Shirt")
+
+    def info(self):
+        print("Pants has 3 protection")
 
 
 class Sweater(Clothes):
     def __init__(self):
         super(Sweater, self).__init__("Sweater")
 
+    def info(self):
+        print("Pants has 3 protection")
+
 
 class Socks(Clothes):
     def __init__(self):
         super(Socks, self).__init__("Socks")
+
+    def info(self):
+        print("Pants has 3 protection")
 
 
 class Potion(Item):
@@ -278,16 +315,36 @@ class Potion(Item):
         self.protection = 50
         self.damage = 50
         self.usage_left = 100
+        self.life = 50
+        self.power = 50
+
+    def drink(self):
+        self.usage_left -= 1
+        self.life += 1
+        print("You take the potion and you have %s life now and %d is how much is left" % (self.life,
+                                                                                           self.usage_left))
 
 
 class LifeP(Potion):
     def __init__(self):
         super(LifeP, self).__init__("Life Potion")
 
+    def drink(self):
+        self.usage_left -= 1
+        self.life += 1
+        print("You take the life potion and you have %s life now and %d is how much is left" % (self.life,
+                                                                                                self.usage_left))
+
 
 class Power(Potion):
     def __init__(self):
         super(Power, self).__init__("Power Potion")
+
+    def drink(self):
+        self.usage_left -= 1
+        self.power += 1
+        print("You take the power potion and you have %s life now and %d is how much is left" % (self.power,
+                                                                                                 self.usage_left))
 
 
 class Food(Item):
@@ -295,38 +352,64 @@ class Food(Item):
         super(Food, self).__init__(name)
         self.food_left = 100
         self.damage = 100
+        self.life = 100
+
+    def eat(self):
+        self.food_left -= 10
+        self.life += 1
+        print("You eat and have %s life now and has %d food left" % (self.food_left, self.life))
 
 
 class Chicken(Food):
     def __init__(self):
         super(Chicken, self).__init__("Chicken")
 
+    def eat(self):
+        self.food_left -= 10
+        self.life += 1
+        print("You eat Chicken and have %s life now and has %d food left" % (self.food_left, self.life))
+
 
 class Pork(Food):
     def __init__(self):
         super(Pork, self).__init__("Pork")
 
-
-class SeaFood(Food):
-    def __init__(self):
-        super(SeaFood, self).__init__("Sea Food")
+    def eat(self):
+        self.food_left -= 10
+        self.life += 1
+        print("You eat Pork and have %s life now and has %d food left" % (self.food_left, self.life))
 
 
 class Eggs(Food):
     def __init__(self):
         super(Eggs, self).__init__("Eggs")
 
+    def eat(self):
+        self.food_left -= 10
+        self.life += 1
+        print("You eat Eggs and have %s life now and has %d food left" % (self.food_left, self.life))
+
 
 class Boots(Item):
     def __init__(self, name):
         super(Boots, self).__init__(name)
-        self.protection = 20
+        self.protection = 50
         self.usage_left = 100
+
+    def walk(self):
+        self.usage_left -= 1
+        print("You walk and have %s usage left on your boots" % self.usage_left)
 
 
 class LeatherB(Boots):
     def __init__(self):
         super(LeatherB, self).__init__("Leather Boots")
+        self.protection = 100
+        self.usage_left = 1000
+
+    def walk(self):
+        self.usage_left -= 1
+        print("You walk and have %s usage left on your leather boots" % self.usage_left)
 
 
 class Pills(Item):
@@ -334,16 +417,38 @@ class Pills(Item):
         super(Pills, self).__init__(name)
         self.damage = 50
         self.life = 20
+        self.usage_left = 10
+
+    def take(self):
+        self.life += 1
+        self.usage_left -= 1
+        print("You tak the pil and have %s life added to you and have %d pills left" % (self.life, self.usage_left))
 
 
 class Energy(Pills):
     def __init__(self):
         super(Energy, self).__init__("Energy Pills")
+        self.energy = 100
+        self.usage_left = 5
+
+    def take(self):
+        self.energy += 1
+        self.usage_left -= 1
+        print("You tak the power pill and have %s power added to you and have %d pills left" %
+              (self.energy, self.usage_left))
 
 
 class Strength(Pills):
     def __init__(self):
         super(Strength, self).__init__("Strength Pills")
+        self.strength = 100
+        self.usage_left = 5
+
+    def take(self):
+        self.strength += 1
+        self.usage_left -= 1
+        print("You tak the strength pill and have %s power added to you and have %d pills left" %
+              (self.strength, self.usage_left))
 
 
 class Axe(Item):
@@ -352,15 +457,31 @@ class Axe(Item):
         self.damage = 80
         self.usage_left = 100
 
+    def attack(self):
+        self.usage_left -= 10
+        print("You attacked with the axe and have %s usage left" % self.usage_left)
+
 
 class Ironaxe(Axe):
     def __init__(self):
         super(Ironaxe, self).__init__("Steel Axe")
+        self.damage = 90
+        self.usage_left = 1000
+
+    def attack(self):
+        self.usage_left -= 10
+        print("You attacked with the iron axe and have %s usage left" % self.usage_left)
 
 
 class Steelaxe(Axe):
     def __init__(self):
         super(Steelaxe, self).__init__("Steel Axe")
+        self.damage = 100
+        self.usage_left = 10000
+
+    def attack(self):
+        self.usage_left -= 10
+        print("You attacked with the steel axe and have %s usage left" % self.usage_left)
 
 
 class Water(Item):
@@ -368,16 +489,37 @@ class Water(Item):
         super(Water, self).__init__(name)
         self.health = 10
         self.strength = 10
+        self.usage_left = 100
+
+    def drink(self):
+        self.health += 5
+        self.strength += 5
+        self.usage_left -= 10
+        print("You have drink the water you have %s health added, %s strength added and you have %s water left" %
+              (self.health, self.strength, self.usage_left))
 
 
 class Cold(Water):
     def __init__(self):
         super(Cold, self).__init__("Cold Water")
+        self.health = 10
+        self.strength = 10
+        self.usage_left = 100
+
+    def drink(self):
+        self.health += 10
+        self.strength += 10
+        self.usage_left -= 10
+        print("You have drink the water you have %s health added, %s strength added and you have %s water left" %
+              (self.health, self.strength, self.usage_left))
 
 
 class Hot(Water):
     def __init__(self):
         super(Hot, self).__init__("Hot Water")
+        self.health = 10
+        self.strength = 10
+        self.usage_left = 100
 
 
 enemy_attack = Knife("Knife")
