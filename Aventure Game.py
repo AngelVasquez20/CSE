a5c1dac438d26af2853431da6b15e4eebd86cac8
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-03-18 14:38:26 -0700

    3/18/19

--- Aventure Game.py
+++ Aventure Game.py
@@ -1,3 +1,45 @@
+class Room(object):
+    def __init__(self, name, description, north=None, west=None, east=None, south=None, up=None, down=None):
+        self.name = name
+        self.description = description
+        self.north = north
+        self.west = west
+        self.east = east
+        self.south = south
+        self.up = up
+        self.down = down
+
+
+Your_office = Room("Your office", "This is your work office", None, "Weight_Room", "Clue_Room", "Co_Worker_office",
+                   None, None)
+Co_Worker_office = Room("Co-worker's office", "This is your co-worker's office", "Your_office", "Break_Room2",
+                        "Break_Room", "Clue_Room2", None, None)
+Clue_Room = Room("Clue room", "This a clue room", None, "Your_office", None, "Break_Room", None, None)
+Weight_Room = Room("Weight room", "This is where the workers workout", None, None, "Your_office", "Break_Room", None,
+                   None)
+Break_Room = Room("Break Room", "This is where the workers take breaks", "Clue_Room", "Co_Worker_Office", None,
+                  "Training_Room", None, None)
+Training_Room = Room("training room", "This is where workers do their training", None, "Clue_Room2", None, None, None,
+                     None)
+Clue_Room2 = Room("Another clue room", "This is where another clue is at", "Co_Worker_office", "Clue_Room3",
+                  "Training_Room", "Main_Office", None, None)
+Clue_Room3 = Room("third clue room", "This is where the third clue is at", "Break_Room2", None, "Clue_Room2",
+                  "Clue_Room4", None, None)
+Break_Room2 = Room("Another break room", "This is another room where the workers take brake", "Weight_Room", None,
+                   "Co_Worker_office", None, None, None)
+Clue_Room4 = Room("4th clue room", "This is where the 4th clue is at", "Clue_Room3", None, "Main_Office", "Office",
+                  None, None)
+Office = Room("A Office", "This is a random office", "Clue_Room4", None, "Main_Office", None, None, None)
+Back_office = Room("Back office", "This is the back of the main office", None, "Main_Office", None, "Parking_Lot",
+                   None, None)
+Main_Office = Room("Main office", "This is the main office", "Clue_Room2", "Office", "Back_office", "Front_Door",
+                   None, None)
+Front_Door = Room("Front door", "This is the front door either for the entrance or the exit", "Main_Office", None,
+                  "Parking_Lot", None, None, None)
+Parking_Lot = Room("Parking lot", "This is where all the worker's cars are parked at", "Back_office", "Front_Door",
+                   None, None, None, None)
+
+
 class Item(object):
     def __init__(self, name):
         self.name = name
@@ -534,3 +576,57 @@ enemy_attack.attack()
 
 angel_shoot = Tasergun()
 angel_shoot.shoot()
+
+
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.inventory = []
+        self.current_location = starting_location
+
+    def move(self, new_location):
+        """This method moves a player to a new location
+
+        :param new_location: The room object that we move to
+        :return:
+        """
+        self.current_location = new_location
+
+    def find_room(self, direction):
+        """This method takes a direction, and finds the variable of the room.
+
+        :param direction: A String (all lowercase), with a cardinal direction
+        :return: A rom object if it exist, None if it does not
+        """
+        return getattr(self.current_location, direction)
+
+
+class Boss(object):
+    def __init__(self):
+        self.health = 100
+        self.strength = 100
+
+
+player = Player(Your_office)
+
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
+playing = True
+
+# Controller
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_ ")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            next_room = player.find_room(command)
+            if next_room is None:
+                raise KeyError
+            player.move(next_room)
+        except KeyError:
+            print("I can't go that way")
+
+    else:
+        print("Command not recognized")
