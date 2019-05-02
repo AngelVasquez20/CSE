abf45145a230b182515716eb0bf8ea7ceba9169a
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-05-02 14:25:43 -0700

    5/2/19

--- Angel Vasquez - Validator.py
+++ Angel Vasquez - Validator.py
@@ -1,24 +1,31 @@
-test_num = "4712839550684070"
+test_num = "4556737586899855"
 print(test_num)
+
+
+def divisible_by_even_num(num: str):
+    even_num = int(num[::2])
+    if even_num
+
 
 
 def remove_num(string):
     return string[:15]
 
 
-remove_num(test_num[:15])
-
-print(remove_num(test_num))
-
-
 def reverse_it(string):
     return string[::-1]
 
 
-print(reverse_it(remove_num(test_num)))
-
-
 def validate(num: str):
-    for index in range(len(num)):
-        int_version = int(num[index])
-        print(validate(test_num))
+    cut_nums = remove_num(test_num[:15])
+    # print(cut_nums)
+    reversed_nums = reverse_it(cut_nums)
+    # print(reversed_nums)
+    list_nums = list(reversed_nums)
+    print(list_nums)
+    for index in range(len(list_nums)):
+        list_nums[index] = int(list_nums[index])
+        print(list_nums)
+
+
+print(validate(test_num))
--- notes for validator.py
+++ notes for validator.py
@@ -14,3 +14,20 @@ print(5 - 3)
 print(4 * 5)
 print(6 / 5)
 print()
+
+
+def divisible_by_even_num(num: str):
+    even_num = int(num[0])  # This is the first number
+    if even_num % 2 == 0:
+        return True
+    return False
+    if divisible_by_even_num(num) is :
+        return True
+    return False
+
+
+def divisible_by_even_num(num: str):
+    even_num = int(num[0])  # This is the first number
+    if even_num in validate(num) * 2 == 0:
+        return True
+    return False
