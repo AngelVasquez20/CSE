536e21d716fb67a7b4cd409832e10dddf6079c47
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-05-06 14:39:18 -0700

    5/6/19

--- notes for validator.py
+++ notes for validator.py
@@ -5,3 +5,12 @@ print(4 * 5)
 print(6 / 5)
 print()
 
+
+def multiply_even_num(num: list):
+    for index in range(len(num)):
+        num[index] = int(num[index])
+        if index % 2 == 0:
+            num[index] *= 2
+            if num[index] > 9:
+                num[index] -= 9
+
