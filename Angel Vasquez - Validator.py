8e5b0ef4312e6f9ac6ec9815f4d9cb1e8668b7cf
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-05-07 14:42:06 -0700

    2/7/19

--- Angel Vasquez - Validator.py
+++ Angel Vasquez - Validator.py
@@ -10,22 +10,23 @@ def reverse_it(string):
     return string[::-1]
 
 
-print(reverse_it(remove_num(test_num)))
-
-
 def validate(num: str):
-    for index in range(len(num)):
-        int_version = int(num[index])
-        print(validate(test_num))
     cut_nums = remove_num(test_num[:15])
-    # print(cut_nums)
     reversed_nums = reverse_it(cut_nums)
-    # print(reversed_nums)
     list_nums = list(reversed_nums)
     print(list_nums)
     for index in range(len(list_nums)):
         list_nums[index] = int(list_nums[index])
-        print(list_nums)
+        print(list_nums[index])
+
+
+def multiply_even_num(num: list):
+    for index in range(len(num)):
+        num[index] = int(num[index])
+        if index % 2 == 0:
+            num[index] *= 2
+            if num[index] > 9:
+                num[index] -= 9
 
 
 print(validate(test_num))
